59ed481a4e98c42ab86ec5c1dfffc7723f2581a2
DoudGaya <arbdoule@yahoo.com>
2023-05-04 16:39:31 +0100

    dreeo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+ 
+  return (
+    <>
+          <Script id="google-analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-73SHRQY3GQ');
+        `}
+        </Script>
+        <Script async  src="https://www.googletagmanager.com/gtag/js?id=G-73SHRQY3GQ"></Script>
+  
+      <Component {...pageProps} />
+    </>
+    
+    )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,17 +4,7 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-          
       <Head />
-      <Script id="google-analytics" strategy="afterInteractive">
-      {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-73SHRQY3GQ');
-      `}
-      </Script>
-      <Script async  strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-73SHRQY3GQ"></Script>
       <body>
         <Main />
         <NextScript />
