3a07877ad71447dc0cde4e0f1ea1e693952bc984
DoudGaya <arbdoule@yahoo.com>
2023-05-22 17:21:39 +0100

    new landing

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,24 +1,30 @@
-import landing from '../public/landing.jpg'
 import Link from 'next/link'
-
+import Image from 'next/image'
+import landings from '..//public/small.png'
 export const Landing = () => {
     return (
         <>
-        <div className=" h-screen flex  w-full bg-cover " style={ { backgroundImage: `url(${landing.src})`} } >
-            <div className=" w-full h-full bg-gradient-to-b backdrop-blur-sm from-black/30 via-slate-800/30 to-slate-900/30 flex flex-col justify-center  items-center">
-             <div className=" xl:w-[1200px] w-full h-full flex flex-col justify-center text-center ">
-                <div className=" space-y-6 ">
-                    <div className=" xl:text-6xl text-4xl text-white font-semibold">Welcome to your professional network</div>
-                    <div className=" xl:text-5xl text-white text-2xl">Share, Connect and Promotes</div>
-                </div>  
-                <div className=" flex flex-col xl:flex-row xl:space-x-6 justify-center pt-10 px-10 xl:space-y-0 space-y-8">
-                    <Link href={'#'} className=' py-3 px-6 text-white hover:bg-slate-800/40 hover:text-white border-slate-100 border-2  font-bold text-xl'>
-                       Community
+        <div className=" lg:h-screen flex  w-full bg-cover">
+            <div className=" w-full h-full bg-gradient-to-r backdrop-blur-sm from-black via-black to-slate-900 flex flex-col justify-center items-center">
+             <div className=" xl:w-[1200px] w-full h-full grid gap-y-10 lg:gap-y-0 grid-cols-1 py-16 lg:py-0 lg:grid-cols-2 justify-center text-center lg:text-start">
+                <div className=" space-y-6 flex lg:items-start justify-center flex-col px-4 lg:px-0">
+                    <div className="xl:text-5xl text-3xl text-white font-semibold">Welcome to <span className=' text-blue-400 font-logo'>Chemcider</span> your professional network</div>
+                    <div className="xl:text-2xl text-white text-xl">Share, Connect and Promotes</div>
+                    <div className=" flex flex-col xl:flex-row xl:space-x-6 justify-center pt-10 px-10 lg:px-0 xl:space-y-0 space-y-8">
+                    <Link href={'#'} className=' py-3 px-10 text-white hover:bg-slate-800/40 hover:text-white border-slate-100 border-2  font-bold text-xl'>
+                       Articles
                     </Link>
                     <Link href={'#'} className=' py-3 px-6 text-white bg-slate-900 text-xl border-slate-900 border-2 font-bold '>
-                        Resource Store
+                        Jobs
                     </Link>
                 </div>
+                </div>
+                <div className=" h-full flex items-center">
+                    <div className=" gap-4 overflow-hidden w-full px-6">
+                       <Image src={landings} className=' drop-shadow-xl w-full h-full' alt='Chemcider HOME' />
+                    </div>
+                </div>
+             
              </div>
             </div>
         </div>
--- components/WhoWeAre.tsx
+++ components/WhoWeAre.tsx
@@ -1,41 +1,46 @@
 import engineer from '../public/engineer.jpg'
 import doctor from '../public/doctor.jpg'
 import scientist from '../public/scientist.jpg'
+import info from '../public/info.jpg'
+import connect from '../public/connect.jpg'
 import Image from 'next/image'
 
 
 export const WhatWeDo = () => {
     return (
         <>
-        <div className=" xl:h-[500px] flex flex-col items-center ">
-            <div className=" grid xl:grid-cols-3 grid-cols-1 h-full">
+        <div className=" flex flex-col items-center ">
+            <div className="grid grid-cols h-full">
             
-            
-            <div className=" dark:bg-slate-900 px-10 flex justify-center leading-7 py-16 xl:px-4 flex-col ">
-                <h2 className='text-md lg:text-lg py-2 text-start text-xl  font-logo'>Our Mission</h2>
+            <div className=" dark:bg-slate-900 grid lg:grid-cols-2 grid-cols-1 px-10 justify-center leading-7 xl:px-0 flex-col ">
+               <div className=" px-6 flex flex-col justify-center py-6">
+               <h2 className='text-md lg:text-lg py-2 text-start text-xl  font-logo'>Our Mission</h2>
                 <p className=' xl:text-base text-justify '>
                     Our mission is to provide scientists and engineers with access to quality products and connections that will help 
                     them advance their research and projects. 
                     Whether you're looking for cutting-edge technology, trusted suppliers, or like-minded peers, we've got you covered.
                 </p>
+               </div>
+               <div className=" h-[400px] overflow-hidden">
+               <Image src={connect} className=' rounded-bl-full overflow-hidden w-full h-full object-center object-cover' alt='Chemcider Server infomation' />
+               </div>
             </div>
 
-                <div className=" dark:bg-black flex leading-7  py-16 bg-white justify-center px-10 xl:px-4 flex-col ">
-                    <h2 className=' lg:text-lg py-2 text-xl text-start font-logo '>Access to the large scientific community</h2>
-                    <p className=' text-md text-justify'>
-                        We believe that connections are key to success in any field, and that's especially true in science and engineering. 
-                        That's why we're dedicated to providing you with opportunities 
-                        to connect with other scientists and engineers from around the world.
-                    </p>
-                </div>
-
-                <div className="dark:bg-slate-900 flex leading-7 py-16 justify-center px-10 xl:px-4 flex-col ">
-                    <h2 className=' text-xl lg:text-lg py-2 text-start font-logo '>Cutting-Edge Information</h2>
+            <div className=" dark:bg-slate-900 grid grid-cols-1 lg:grid-cols-2 px-10 justify-center leading-7 xl:px-0 flex-col ">
+               <div className=" h-[400px] overflow-hidden">
+               <Image src={info} className=' rounded-tr-full overflow-hidden w-full h-full object-center object-cover' alt='Chemcider Server infomation' />
+               </div>
+               <div className="px-6 flex flex-col justify-center py-6">
+                <h2 className=' text-xl lg:text-lg py-2 text-start font-logo '>Cutting-Edge Information</h2>
                     <p className=' text-md text-justify'>
                         Stay up-to-date with the latest scientific and engineering news and trends with our blog and newsletter.
                         Our experts will provide insights, analysis, and advice on the topics that matter most to you.
                     </p>
-                </div>
+               </div>
+            </div>
+
+
+
 
             </div>
         </div>
