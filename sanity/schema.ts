1a578c8a8a4ce8221f7db5a37845c77fd504a292
DoudGaya <arbdoule@yahoo.com>
2023-08-12 12:45:47 +0100

    applicants

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -5,10 +5,10 @@ import category from './schemas/category'
 import post from './schemas/post'
 import author from './schemas/author'
 import lesson from './schemas/lesson'
-import Applicants from './schemas/Applicants'
+import applicants from './schemas/Applicants'
 
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent, Applicants, lesson],
+  types: [post, author, category, blockContent, applicants, lesson],
 }
