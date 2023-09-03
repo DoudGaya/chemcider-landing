bd6000615b71adf8dfd276d4f41dc8ca2e579ad8
DoudGaya <arbdoule@yahoo.com>
2023-09-03 22:37:58 +0100

    client

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,13 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { client } from "@/sanity/lib/client";
 
-
-const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    useCdn: true,
-}
 export default function createComment(
     req: NextApiRequest,
     res: NextApiResponse
