044e4a6abcf358257b809da29b856467a1a55091
DoudGaya <arbdoule@yahoo.com>
2023-09-03 20:21:15 +0100

    chanegs

--- components/StoreImages.tsx
+++ components/StoreImages.tsx
@@ -8,8 +8,8 @@ const image = productImages.map( item => {
 
 export const StoreImage = () => {
     return (
-        <div className=" flex flex-col items-center justify-center dark:brightness-[90%]">
-            <h2 className=" text-lg md:hidden font-semibold flex text-center ">Resource Store - For Scientists and Engineers </h2>
+        <div className=" flex flex-col py-6 lg:py-6 overflow-x-auto items-center justify-center dark:brightness-[90%]">
+            <h2 className=" md:hidden font-raleway text-2xl  flex text-center ">Chemcider Store </h2>
                 <div className="flex snap-x xl:snap-x-none overflow-x-scroll xl:overflow-x-hidden space-x-6 h-[250px] border-r-2 py-6 border-gray-500 rounded-r-lg overflow-hidden ">
                     {image}
                 </div>
--- components/store/StoreContents.tsx
+++ components/store/StoreContents.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { StoreImage } from '../StoreImages'
 
+
+
+
+
 export const StoreContents = () => {
   return (
-      <div className=" flex flex-col">
-            <div className=" w-full py-20 bg-slate-200 dark:bg-slate-900">
-          <div className=" flex flex-col  mx-auto max-w-5xl">
-              <div className=" flex w-full border-b dark:border-slate-700 my-6 py-4">
+      <div className=" flex flex-col ">
+            <div className=" w-full px-8 py-20 bg-slate-200 dark:bg-slate-900">
+          <div className=" flex flex-col  mx-auto max-w-5xl space-y-8">
+              <div className=" flex w-full justify-center border-b dark:border-slate-700 ">
                    <h3 className=' text-2xl font-raleway'>The Chemcider Store</h3>
            </div>
-              <div className="  grid grid-cols-2 gap-x-6  my-auto">
+              <div className="  grid grid-cols-1 lg:grid-cols-2 gap-x-6">
               <div className=" h-full my-auto flex flex-col justify-center">
-                <p className=' font-poppin text-justify'>
+                <p className=' font-poppin text-lg  text-justify'>
                   At Chemcider Store, we are your one-stop destination for high quality laboratory equipment,
                   essential medical supplies, and top-notch chemicals in Nigeria. With a commitment to excellence
                   and a passion for serving healthcare professionals, researchers, and institutions, we provide a
@@ -25,8 +29,15 @@ export const StoreContents = () => {
     </div>
           </div>
           
-          <div className="">
-              Even more contents 
+          <div className=" w-full py-20">
+              <div className=" mx-auto max-w-5xl flex flex-col w-full">
+                  <div className=" flex w-full justify-center ">
+                      <h3 className=' font-raleway text-center text-2xl '>Explore Our Extensive Range of Products</h3>
+                  </div>
+                  <div className=" grid grid-cols-3">
+                      
+                  </div>
+              </div>
           </div>
   </div>
   )
